0877c7a08069c762230d3507f0f10f2e418cac01
renjer <mranjbar.z2993@gmail.com>
2018-11-28 01:54:11 +0330

    Add webhook

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.post('/update', function (req, res) {
 console.log("Listening port : ", process.env.PORT ||443)
 function sendMessage(id, message) {
    // return axios.get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${id}&text=${querystring.stringify(message)}&parse_mode=Markdown`)
-   return axios.get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${id}&text=${message}&parse_mode=Markdown`)
+   return axios.post(`https://api.telegram.org/bot${token}/sendMessage`,
+       {
+           chat_id :id,
+           text : message,
+           parse_mode:Markdown
+       })
 }
 app.listen(process.env.PORT ||3000)
