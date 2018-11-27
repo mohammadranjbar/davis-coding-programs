d3ccbf673298d074decc95691ee3c35e41edeb8d
renjer <mranjbar.z2993@gmail.com>
2018-11-28 01:26:57 +0330

    Add webhook

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '766202188:AAHytXhH9sg6_JzcH0XAZg3Z_f40rmhAhqU';
-
-// const bot = new TelegramBot(token, {polling: true});
 const converter = require('./convertor')
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on('message', (msg) => {
-//     const chatId = msg.chat.id;
-//     const text = msg.text
-//    if (Number(text) ==0 || Number(text)){
-//         bot.sendMessage(chatId, "```\n" + converter.getFullCoding(Number(text))+"\n```",{parse_mode:'Markdown'});
-//     }else{
-//        bot.sendMessage(chatId,'Send me a number, I will send you the programing of this number',{parse_mode:'Markdown'})
-//    }
-//     console.log('msg : ', msg.from)
-//     // send a message to the chat acknowledging receipt of their message
-//
-// });
-// bot.getMe(result => {
-//     console.log("getMe() : ", result)
-// })
-
 
 var express = require('express')
 const bodyParser = require('body-parser')
@@ -36,8 +12,10 @@ app.post('/update', function (req, res) {
     const chatId = message.chat.id
     const text = message.text
     if (Number(text) ==0 || Number(text)){
-        sendMessage(chatId, "Let me think, I should calculate and send you");
-        sendMessage(chatId, "```\n" + converter.getFullCoding(Number(text))+"\n```",{parse_mode:'Markdown'});
+        sendMessage(chatId, "Let me think, I should calculate and send result you ....")
+            .then((result ) => {
+                sendMessage(chatId, "```\n" + converter.getFullCoding(Number(text))+"\n```",{parse_mode:'Markdown'});
+            })
     }else{
         sendMessage(chatId,'Send me a number, I will send you the programing of this number',{parse_mode:'Markdown'})
     }
